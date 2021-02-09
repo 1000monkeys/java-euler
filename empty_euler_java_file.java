6122cd5271c642eaddba867ae35d41cf67bb0734
Kjell Vos <vos.kjell@gmail.com>
2021-02-09 02:05:16 +0100

    fixed tabs v2

--- empty_euler_java_file.java
+++ empty_euler_java_file.java
@@ -1,11 +1,15 @@
 import java.util.concurrent.TimeUnit;
 
+/*
+    Made by Kjell Vos
+    vos.kjell@gmail.com
+*/
 class Main {
-	public static void main(String[] args){
+    public static void main(String[] args){
         final long startTime = System.nanoTime();
 
         // code
-        
+ 
         final long endTime = System.nanoTime();
         System.out.println("Total execution time in nano seconds: " +
                            (endTime - startTime)
@@ -13,5 +17,5 @@ class Main {
         System.out.println("Total execution time in milli seconds: " +
                            TimeUnit.NANOSECONDS.toMillis(endTime - startTime)
         );
-	}
+    }
 }
