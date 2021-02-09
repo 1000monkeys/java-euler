79183cf2cd793dfbd9d8007ad895f0ff3a52c61d
Kjell Vos <vos.kjell@gmail.com>
2021-02-10 00:53:44 +0100

    Problem 8 included

---
+++ make_problem.sh
@@ -0,0 +1,4 @@
+mkdir ~/java-euler/problem$1
+touch ~/java-euler/problem$1/problem$1.java
+cat ~/java-euler/empty_euler_java_file.java >> ~/java-euler/problem$1/problem$1.java
+vim ~/java-euler/problem$1/problem$1.java
