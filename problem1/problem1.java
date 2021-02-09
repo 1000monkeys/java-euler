2e3f17dde37360787260f37f3559f44d08ccf12d
Kjell Vos <vos.kjell@gmail.com>
2021-02-09 01:02:58 +0100

    First Commit

---
+++ problem1/problem1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Main{
+	public static void main(String[] args) {
+		ArrayList<Integer> multiples = new ArrayList<Integer>();
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				multiples.add(i);
+			}
+		}
+		int sum = 0;
+		for (int number : multiples) {
+			sum += number;
+		}
+		System.out.println(sum);
+	}
+}
---
+++ problem2/problem2.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Main {
+	public static void main(String[] args){
+		ArrayList<Integer> sequence = new ArrayList<Integer>();
+        sequence.add(0);
+        sequence.add(1);
+        sequence.add(1);
+        
+        for (int i = 2; sequence.get(i) < 4_000_000; i += 1) {
+            sequence.add(sequence.get(i) + sequence.get(i - 1));
+        }
+
+        long sum = 0;
+        for (int number : sequence) {
+            if (number % 2 == 0) {
+                sum += number;
+            }
+        }
+        System.out.println(sum);
+	}
+}
