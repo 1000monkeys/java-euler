cbbbc1e335a71e6102aa56dd05d14a4a079306e6
Kjell Vos <vos.kjell@gmail.com>
2021-02-10 02:55:05 +0100

    Improved execution speed problem 1

--- problem1/problem1.java
+++ problem1/problem1.java
@@ -10,16 +10,12 @@ class Main{
     public static void main(String[] args) {
         final long startTime = System.nanoTime();
 
+        int sum = 0;
         ArrayList<Integer> multiples = new ArrayList<Integer>();
         for (int i = 0; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
-                multiples.add(i);
+                sum += i;
             }
-        }
-
-        int sum = 0;
-        for (int number : multiples) {
-            sum += number;
         }
         System.out.println(sum);
 
